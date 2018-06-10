23985cebbf2ce5fcd7701dff2da31cd64c351830
Femke Knottenbelt <femknotgithub@gmail.com>
2018-06-10 07:45:13 +0000

    Add chapter 3 exercise 1

---
+++ 120/book/3_classes_and_objects/exc1.rb
@@ -0,0 +1,36 @@
+class MyCar
+  attr_accessor :color, :model, :speed
+  attr_reader :year
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+
+  def self.milage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def speedup(n)
+    self.speed += n
+  end
+
+  def brake(n)
+    self.speed -= n
+  end
+
+  def shutoff
+    self.speed = 0
+  end
+
+  def spray_paint(new_color)
+    self.color = new_color
+  end
+
+
+end
+
+#car = MyCar.new(2018, 'red', 'Ford')
+MyCar.milage(13, 351)
