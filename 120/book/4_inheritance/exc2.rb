d2e798c34d77115c095476b4c1ae3f1d4a2eebab
Femke Knottenbelt <femknotgithub@gmail.com>
2018-06-11 06:50:58 +0000

    Add exercise 2 chapter 4

---
+++ 120/book/4_inheritance/exc2.rb
@@ -0,0 +1,68 @@
+
+module Fuelable
+  def milage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+class Vehicle
+  @@no_of_vehicles = 0
+
+  attr_accessor :color, :model, :speed
+  attr_reader :year
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+    @@no_of_vehicles += 1
+  end
+
+  def spray_paint(new_color)
+    self.color = new_color
+  end
+
+  def self.no_of_vehicles
+    puts "there are #{@@no_of_vehicles} vehicles"
+  end
+end
+
+class MyCar < Vehicle
+  NO_OF_DOORS = 4
+  include Fuelable
+
+  def to_s
+    puts "This car is a #{model} from #{year} and has the color #{color}"
+  end
+
+  def speedup(n)
+    self.speed += n
+  end
+
+  def brake(n)
+    self.speed -= n
+  end
+
+  def shutoff
+    self.speed = 0
+  end
+
+end
+
+class MyTruck < Vehicle
+  NO_OF_DOORS = 2
+  include Fuelable
+
+  def to_s
+    puts "This truck is a #{model} from #{year} and has the color #{color}"
+  end
+end
+
+car = MyCar.new(2018, 'red', 'Ford')
+truck = MyTruck.new(2010, 'blue', 'Toyota')
+car.milage(13, 351)
+truck.milage(5, 351)
+puts car
+puts truck
+Vehicle.no_of_vehicles
