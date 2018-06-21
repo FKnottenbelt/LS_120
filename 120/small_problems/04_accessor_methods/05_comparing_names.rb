749fa4c8c638c3578a470238860387746a6072a5
Femke Knottenbelt <femknotgithub@gmail.com>
2018-06-21 20:29:44 +0000

    Add problem 4 to 7 sector 4

---
+++ 120/small_problems/04_accessor_methods/05_comparing_names.rb
@@ -0,0 +1,38 @@
+# Using the following code, add the appropriate accessor methods. Keep in mind
+# that @last_name shouldn't be visible outside the class.
+#
+# class Person
+#   def first_equals_last?
+#     first_name == last_name
+#   end
+# end
+#
+# person1 = Person.new
+# person1.first_name = 'Dave'
+# person1.last_name = 'Smith'
+# puts person1.first_equals_last?
+#
+# Expected output:
+#
+# false
+
+class Person
+  attr_accessor :first_name
+  attr_writer :last_name
+
+  def first_equals_last?
+    first_name == last_name
+  end
+
+  private
+  attr_reader :last_name
+  # def last_name
+  #   @last_name
+  # end
+end
+
+person1 = Person.new
+person1.first_name = 'Dave'
+person1.last_name = 'Smith'
+puts person1.first_equals_last?
+#p person1.last_name
