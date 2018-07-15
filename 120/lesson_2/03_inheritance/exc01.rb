e2e0f432ef58b1b0b818d30292f82f9ef25dd785
Femke Knottenbelt <femknotgithub@gmail.com>
2018-07-15 11:18:29 +0000

    Add lesson 2-3, exercise 1

---
+++ 120/lesson_2/03_inheritance/exc01.rb
@@ -0,0 +1,51 @@
+=begin
+Suppose we're building a software system for a pet hotel business, so our
+classes deal with pets.
+
+    Given this class:
+
+    class Dog
+      def speak
+        'bark!'
+      end
+
+      def swim
+        'swimming!'
+      end
+    end
+
+    teddy = Dog.new
+    puts teddy.speak           # => "bark!"
+    puts teddy.swim           # => "swimming!"
+
+One problem is that we need to keep track of different breeds of dogs,
+since they have slightly different behaviors. For example, bulldogs
+can't swim, but all other dogs can.
+
+Create a sub-class from Dog called Bulldog overriding the swim method
+to return "can't swim!"
+
+=end
+
+class Dog
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+end
+
+class Bulldog < Dog
+  def swim
+    "can't swim!"
+  end
+end
+
+teddy = Dog.new
+puts teddy.speak == "bark!"
+puts teddy.swim == "swimming!"
+buddy = Bulldog.new
+puts buddy.speak == "bark!"
+puts buddy.swim == "can't swim!"
