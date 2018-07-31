8055057f475b896b09788e0075108809c4fd4dd9
Femke Knottenbelt <femknotgithub@gmail.com>
2018-07-31 06:52:56 +0000

    Add lesson 4-1-7

---
+++ 120/lesson_4/easy1/q7.rb
@@ -0,0 +1,14 @@
+=begin
+What is the default return value of to_s when invoked on an object?
+Where could you go to find out if you want to be sure?
+=end
+
+# A
+# the object
+
+class Person
+end
+
+bob = Person.new
+puts bob
+#<Person:0x000000023be898>
