8e49d94ba4208543f66c261febfc68f106ccb72e
Femke Knottenbelt <femknotgithub@gmail.com>
2018-07-31 18:55:24 +0000

    Add lesson 4-2-5

---
+++ 120/lesson_4/easy2/q5.rb
@@ -0,0 +1,13 @@
+=begin
+There are a number of variables listed below. What are the different
+types and how do you know which is which?
+
+excited_dog = "excited dog"
+@excited_dog = "excited dog"
+@@excited_dog = "excited dog"
+
+=end
+
+excited_dog = "excited dog"  #local var
+@excited_dog = "excited dog"  # either instance var or class instance var
+@@excited_dog = "excited dog" # class var
