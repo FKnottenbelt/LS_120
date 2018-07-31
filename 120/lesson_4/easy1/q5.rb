15e2a1078c260cca2224a45f868ccba3b432d1e7
Femke Knottenbelt <femknotgithub@gmail.com>
2018-07-31 06:42:22 +0000

    Add lesson 4-1-5

---
+++ 120/lesson_4/easy1/q5.rb
@@ -0,0 +1,37 @@
+=begin
+Which of these two classes has an instance variable and how do you know?
+
+class Fruit
+  def initialize(name)
+    name = name
+  end
+end
+
+class Pizza
+  def initialize(name)
+    @name = name
+  end
+end
+=end
+
+
+
+class Fruit
+  def initialize(name)
+    name = name
+  end
+end
+
+class Pizza
+  def initialize(name)
+    @name = name
+  end
+end
+
+hot_pizza = Pizza.new("cheese")
+orange    = Fruit.new("apple")
+
+p hot_pizza.instance_variables
+p orange.instance_variables
+# A
+# Pizza, because it has a @
