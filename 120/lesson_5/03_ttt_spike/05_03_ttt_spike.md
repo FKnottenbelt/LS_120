0f82b976ed742c29dd317de98a63cf32a9821809
Femke Knottenbelt <femknotgithub@gmail.com>
2018-08-02 07:13:53 +0000

    Add TTT step 1

--- 120/lesson_5/03_ttt_spike/05_03_ttt_spike.md
+++ 120/lesson_5/03_ttt_spike/05_03_ttt_spike.md
@@ -177,6 +177,9 @@ game.play
 Watch and type along with the below videos.
 
 Step 1 - Display a board
+make your square 5 spaces wide and 3 high
+move your X onto 3rd space
+
 Step 2 - Setup Board and Square classes
 Step 3 - Human moves
 Step 4 - Computer moves
@@ -185,3 +188,7 @@ Step 6 - Break when board is full
 Step 7 - Detect winner
 Step 8 - Refactor detect_winner
 Step 9 - Play again
+At about 08:12 into this video, the instructor enters a
+system 'clear' statement after display_welcome_message. The
+order of these two statements should be reversed: the
+system 'clear' should come first.
--- 120/lesson_5/03_ttt_spike/ttt.rb
+++ 120/lesson_5/03_ttt_spike/ttt.rb
@@ -17,17 +17,47 @@ class Player
 end
 
 class TTTGame
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe"
+    puts
+  end
+
+  def display_goodbye_message
+    puts "Thank you for playing Tic Tac Toe! Goodbye!"
+  end
+
+  # rubocop: disable Metrics/AbcSize
+  def display_board(brd)
+#    clear_screen
+#    puts "You are a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+    puts ""
+    puts "     |     |"
+#    puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+#    puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+#    puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+    puts "     |     |"
+    puts ""
+  end
+  # rubocop: enable Metrics/AbcSize
+
   def play
     display_welcome_message
     loop do
-      display_board
+      display_board('a')
+      break
       first_player_moves
       break if someone_won? || board_full?
 
       second_player_moves
       break if someone_won? || board_full?
     end
-    display_result
+#    display_result
     display_goodbye_message
   end
 end
