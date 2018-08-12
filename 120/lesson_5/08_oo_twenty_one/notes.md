00e0c33aa5879add5bb794b8e9f04ab1adb57764
Femke Knottenbelt <femknotgithub@gmail.com>
2018-08-12 09:29:29 +0000

    Add dealer-turn

--- 120/lesson_5/08_oo_twenty_one/notes.md
+++ 120/lesson_5/08_oo_twenty_one/notes.md
@@ -5,9 +5,9 @@ remove no_cards_in_deck
 
 should player turn be in game?
 
-- in player turn: if player goes bust, we stop, but it would be nice
- to tell him
-
-- after initial deal, someone might be bust allready.
 
 -> make better play loop than given!
+
+- after initial deal, see if someone is bust allready
+
+- new match y/n
--- 120/lesson_5/08_oo_twenty_one/twenty_one.rb
+++ 120/lesson_5/08_oo_twenty_one/twenty_one.rb
@@ -1,4 +1,6 @@
 module Gameable
+  LINE_WIDTH = 80
+
   def play_again?
     answer = ''
     loop do
@@ -19,12 +21,22 @@ module Gameable
     array << array.pop(2).join(' ' + word + ' ')
     array.join(delimiter)
   end
+
+  def multi_line_prompt(messages_array)
+    puts
+    messages_array.each do |line|
+      puts line.center(LINE_WIDTH)
+    end
+    puts
+  end
 end
 
 module Hand
   BUST = 21
 
   include Gameable
+
+  attr_writer :cards
 
   def cards
     @cards ||= []
@@ -101,6 +113,8 @@ class Player
 end
 
 class Dealer
+  DEALER_MAX = 17
+
   include Hand
 
   def initialize
@@ -122,6 +136,14 @@ class Dealer
 
   def show_hand
     puts "Dealer has: #{hand}"
+  end
+
+  def show_one_card
+    puts "Dealer has #{cards.sample}"
+  end
+
+  def hit_or_stay
+    total <= DEALER_MAX ? 'hit' : 'stay'
   end
 end
 
@@ -194,41 +216,59 @@ class Card
 end
 
 class Game
+  MATCH = 2
   include Gameable
 
-  attr_reader :player, :dealer, :deck
+  attr_reader :player, :dealer, :score
+  attr_accessor :deck
 
   def initialize
     @deck = Deck.new
     @dealer = Dealer.new
     @player = Player.new
+    @score = { player: 0, dealer: 0 }
   end
 
-  def start
-    deal_cards
-    show_initial_cards
-    player_turn
+  def start # match
+    clear_screen
+    # display welcome
+    puts
+    puts "Welcome to TwentyOne"
 
-    if player.bust?
-      puts "Oeps, you got a #{player.cards.last} and " \
-      "went bust... Dealer wins."
-      #score[:dealer] += 1
-      #show_score(score)
-    else
-      dealer_turn(deck, dealer)
-      if bust?(dealer)
-        puts 'Dealer went bust! You won!'
-        score[:player] += 1
-        show_score(score)
+    loop do # round
+      deal_cards
+      show_initial_dealer_card
+
+      player_turn
+
+      if player.bust?
+        puts
+        puts "Oeps, you got a #{player.cards.last} and " \
+        "went bust... Dealer wins."
+        score[:dealer] += 1
+        show_score
       else
-        winner = declare_round_winner(player, dealer)
-        update_score(score, winner)
-        display_round_winner(player, dealer, winner, score)
+        dealer_turn
+        if dealer.bust?
+          puts 'Dealer went bust! You won!'
+          score[:player] += 1
+          show_score
+        else
+         winner = declare_round_winner
+         update_score(winner)
+         display_round_winner(winner)
+        end
       end
-    end
 
-    # dealer_turn
-    # show_result
+      break if score.values.include?(MATCH) # somebody won
+      break unless play_again?
+
+      reset
+    end # round
+
+    winner = declare_match_winner
+    display_match_winner(winner)
+    puts "Thank you for playing Twenty-One. Good bye!"
   end
 
   private
@@ -240,15 +280,13 @@ class Game
     end
   end
 
-  def show_initial_cards
-    puts "Player has: #{player.hand}"
-    puts "Dealer has: #{dealer.hand}"
+  def show_initial_dealer_card
+    dealer.show_one_card
   end
 
   def player_turn
     loop do
       player.show_hand
-      #show_score(score)
       answer = player.ask_hit_or_stay
       if answer == 'h'
         player.add_card(deck.take_card)
@@ -257,6 +295,75 @@ class Game
         break
       end
     end
+  end
+
+  def dealer_turn
+    loop do
+      action = dealer.hit_or_stay
+      if action == 'hit'
+        dealer.add_card(deck.take_card)
+      else
+        break
+      end
+    end
+  end
+
+  def show_score
+    puts "The rounds score is (get #{MATCH} to win): " \
+    "player: #{score[:player]} - dealer: #{score[:dealer]}"
+    puts
+  end
+
+  def reset
+    self.deck = Deck.new
+    player.cards = []
+    dealer.cards = []
+    puts '-----------'
+    puts "Next round!"
+    puts '-----------'
+  end
+
+  def declare_round_winner
+    player_total = player.total
+    dealer_total = dealer.total
+
+    if player_total > dealer_total
+      'player'
+    elsif player_total == dealer_total
+      'tie'
+    else
+      'dealer'
+    end
+  end
+
+  def declare_match_winner
+    if score[:player] == score[:dealer]
+      'tie'
+    else
+      score.key(score.values.max).to_s
+    end
+  end
+
+  def update_score(winner)
+    return if winner == 'tie'
+    score[winner.to_sym] += 1
+  end
+
+  def display_round_winner(winner)
+    message = []
+    message << "The winner of this round is: #{winner}!"
+    message << "score: player: #{player.total} - dealer: #{dealer.total}"
+    message << "rounds score: player: #{score[:player]} - dealer: " \
+               "#{score[:dealer]}"
+    message << "(win #{MATCH} rounds to win a match)"
+    multi_line_prompt(message)
+  end
+
+  def display_match_winner(winner)
+    message = []
+    message << "The winner of this match is: #{winner}!"
+    message << "score: player: #{score[:player]} - dealer: #{score[:dealer]}"
+    multi_line_prompt(message)
   end
 end
 
