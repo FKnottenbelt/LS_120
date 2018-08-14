fe755aff08b712cec1db4c0decafb20c0103681e
Femke Knottenbelt <femknotgithub@gmail.com>
2018-08-14 16:13:20 +0000

    Add further exploration

---
+++ 120/small_problems/07_medium1/06b_number_guesser1.rb
@@ -0,0 +1,12 @@
+=begin
+Further Exploration
+
+You will need the original exercise solution for the next exercise. If you
+work on this Further Exploration, keep a copy of your original solution so
+you can reuse it.
+
+We took a straightforward approach here and implemented a single class. Do
+you think it's a good idea to have a Player class? What methods and data
+should be part of it? How many Player objects do you need? Should you use
+inheritance, a mix-in module, or a collaborative object?
+=end
