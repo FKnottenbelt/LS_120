a44f9d6959d3e84d66b9967e2b5c1e6a374eca4b
Femke Knottenbelt <femknotgithub@gmail.com>
2018-08-15 10:19:19 +0000

    Add 07-08 Further Exploration

---
+++ 120/small_problems/07_medium1/08b_ranking_cards.rb
@@ -0,0 +1,15 @@
+=begin
+Further Exploration
+
+You will need the original exercise solution for the next two exercises.
+If you work on this Further Exploration, keep a copy of your original
+solution so you can reuse it.
+
+Assume you're playing a game in which cards of the same rank are unequal.
+In such a game, each suit also has a ranking. Suppose that, in this game,
+a 4 of Spades outranks a 4 of Hearts which outranks a 4 of Clubs which
+outranks a 4 of Diamonds. A 5 of Diamonds, though, outranks a 4 of Spades
+since we compare ranks first. Update the Card class so that #min and #max
+pick the card of the appropriate suit if two or more cards of the same
+rank are present in the Array.
+=end
