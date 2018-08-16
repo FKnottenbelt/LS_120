362220087f3984cf2270f8a77ea9f2086b9360a8
Femke Knottenbelt <femknotgithub@gmail.com>
2018-08-16 17:14:20 +0000

    Add problem 08-01

---
+++ 120/small_problems/07_medium1/10b_poker.rb
@@ -0,0 +1,22 @@
+=begin
+Further Exploration
+
+The following questions are meant to be thought exercises; rather
+than write code, think about what you would need to do. Feel free
+to write some code after thinking about the problem.
+
+How would you modify this class if you wanted the individual
+classification methods (royal_flush?, straight?, three_of_a_kind?,
+etc) to be public class methods that work with an an Array of 5
+cards, e.g.,
+
+def self.royal_flush?(cards)
+  ...
+end
+
+How would you modify our original solution to choose the best
+hand between two poker hands?
+
+How would you modify our original solution to choose the best
+5-card hand from a 7-card poker hand?
+=end
